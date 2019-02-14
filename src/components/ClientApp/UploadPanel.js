40e05841b8ba2ccd856dc2cb18c9f5e95bba9ae2
Pranav Singhal <pranavsinghal96@gmail.com>
2019-02-14 14:55:04 -0700

    completed upload for for plain text

--- src/components/ClientApp/UploadPanel.js
+++ src/components/ClientApp/UploadPanel.js
@@ -1,16 +1,20 @@
-import React, { PropTypes } from "react";
-import { Form, Button, Row, Col } from "react-bootstrap";
+import React from "react";
+import { Form, Row, Col, Button } from "react-bootstrap";
 export default class UploadPanel extends React.Component {
   constructor(props) {
     super(props);
     this.fileRef = React.createRef();
   }
   state = {
-    clientAppJson: {}
+    clientAppJson: {},
+    formData: []
   };
   componentWillMount() {
     const clientAppJson = JSON.parse(localStorage.getItem("clientAppJson"));
     this.setState({ clientAppJson: clientAppJson });
+  }
+  componentDidMount() {
+    this.addFormFields();
   }
   getDelegates = () => {
     return this.state.clientAppJson.delegateInfo;
@@ -18,15 +22,36 @@ export default class UploadPanel extends React.Component {
   getDataInfo = () => {
     return this.state.clientAppJson.dataInfo;
   };
+  addFormFields = () => {
+    const dataInfo = this.getDataInfo();
+    dataInfo.map((field, id) => {
+      const newField = { fieldName: field.fieldName, fieldValue: "" };
+      this.setState(prevState => ({
+        formData: prevState.formData.concat([newField])
+      }));
+    });
+  };
+  handleChange = id => event => {
+    console.log("handling");
+    const newFormData = this.state.formData.map((field, idx) => {
+      if (idx !== id) return field;
+      return { ...field, fieldValue: event.target.value };
+    });
+    this.setState({ formData: newFormData });
+  };
+  submitForm = () => {};
   generateForm = () => {
     const dataInfo = this.getDataInfo();
     console.log(dataInfo);
-    return dataInfo.map(field => {
+
+    return dataInfo.map((field, id) => {
       console.log(field);
+
       return (
         <Form.Control
           key={field.fieldName}
           placeholder={`enter your ${field.fieldName}`}
+          onChange={this.handleChange(id)}
         />
       );
     });
@@ -38,6 +63,7 @@ export default class UploadPanel extends React.Component {
           <Form>
             <Form.Group controlId="uploadForm">
               {this.generateForm()}
+              <Button onClick={this.submitForm}>Submit</Button>
             </Form.Group>
           </Form>
         </Col>
